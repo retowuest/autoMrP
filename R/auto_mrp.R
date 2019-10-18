4c8b99907b4d7c262524ae7b8dce5cd75f1e21ea
Reto Wuest <reto.wuest@unige.ch>
2019-10-18 07:54:43 +0200

    modify package description

---
+++ R/auto_mrp.R
@@ -0,0 +1,14 @@
+#' Improve MrP through ensemble learning.
+#'
+#' \code{auto_MrP} improves the prediction performance of multilevel regression
+#' with post-stratification (MrP) by combining a number of machine learning
+#' methods through ensemble bayesian model averaging (EBMA).
+#'
+#' @param
+#' @return
+#' @examples
+#' @return
+
+auto_MrP <- function() {
+
+}
