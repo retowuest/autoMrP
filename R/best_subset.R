90bef12f75d17785c27ec2d5a25d3e19b54e1cfd
Reto Wuest <reto.wuest@unige.ch>
2019-10-21 15:42:00 +0200

    add best_subset function

---
+++ R/best_subset.R
@@ -0,0 +1,12 @@
+#' Apply best subset classifier to MrP.
+#'
+#' \code{best_subset} applies the best subset classification method to MrP.
+#'
+#' @param train.data Training data. A data.frame containing the subset of the
+#'   survey data to be used to train the classifier.
+#' @return
+#' @examples
+
+best_subset <- function(train.data) {
+
+}
