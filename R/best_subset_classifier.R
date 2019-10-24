50352fb7b5c8824f9c9e4d32fe31258c4b03508a
Reto Wuest <reto.wuest@unige.ch>
2019-10-24 13:39:33 +0200

    Correct model.family in best subset classifier

--- R/best_subset_classifier.R
+++ R/best_subset_classifier.R
@@ -7,8 +7,8 @@
 #'   model to be estimated on the basis of the provided training data.
 #' @param data.train Training data. A data.frame containing the training data
 #'   used to train the model.
-#' @param model.family Model family. A character-valued scalar indicating the
-#'   model family to be used by glmer. Defaults to "binomial(link = 'probit')".
+#' @param model.family Model family. A variable indicating the model family
+#'   to be used by glmer. Defaults to binomial(link = "probit").
 #' @param model.optimizer Optimization method. A character-valued scalar
 #'   describing the optimization method to be used by glmer. Defaults to
 #'   "bobyqa".
@@ -18,12 +18,12 @@
 #' @examples
 
 best_subset_classifier <- function(model, data.train,
-                                   model.family = "binomial(link = 'probit')",
+                                   model.family = binomial(link = "probit"),
                                    model.optimizer = "bobyqa",
                                    n.iter = 1000000) {
   # Train model on training data
   out <- lme4::glmer(model, data = data.train,
-                     family = get(model.family),
+                     family = model.family,
                      lme4::glmerControl(optimizer = model.optimizer,
                                         optCtrl = list(maxfun = n.iter)))
 
