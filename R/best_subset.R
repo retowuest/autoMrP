b12c07fc3becaafb5291a12b32ca821d32779e14
Reto Wuest <reto.wuest@unige.ch>
2019-10-25 13:32:23 +0200

    add best subset wrapper function

--- R/best_subset.R
+++ R/best_subset.R
@@ -1,12 +1,70 @@
 #' Apply best subset classifier to MrP.
 #'
-#' \code{best_subset} applies the best subset classification method to MrP.
+#' \code{best_subset} is a wrapper function that applies the best subset
+#' classifier to a list of models provided by the user, evaluates the models'
+#' prediction performance, and chooses the best-performing model.
 #'
-#' @param train.data Training data. A data.frame containing the subset of the
-#'   survey data to be used to train the classifier.
+#' @param y Outcome variable. A character scalar containing the column name of
+#'   the outcome variable.
+#' @param L1.x Individual-level covariates. A character vector of column names
+#'   corresponding to the individual-level variables used to predict the outcome
+#'   variable. Must include geographic unit.
+#' @param L2.x Context-level covariates. A character vector of column names
+#'   corresponding to the context-level variables used to predict the outcome
+#'   variable.
+#' @param geo.unit Geographic unit. A character scalar indicating the column
+#'   name of the geographic unit at which outcomes should be aggregated.
+#' @param cv.data Data for cross-validation. A list of k data.frames, one for
+#'   each fold used in k-fold cross-validation.
+#' @param verbose Verbose output.
 #' @return
 #' @examples
 
-best_subset <- function(train.data) {
+best_subset <- function(y, L1.x, L2.x, geo.unit = geo.unit,
+                        cv.data, verbose = FALSE) {
+  # List of all models to be evaluated
+  models <- model_list(y = y, L1.x = L1.x, L2.x = L2.x)
 
+  # Train and evaluate each model
+  m_errors <- lapply(seq_along(models), function(m) {
+    # Print model m
+    if (verbose == TRUE) {
+      M <- length(models)
+      cat(paste("Best subset: Running model ", m,
+                " out of ", M, " models\n", sep = ""))
+    }
+
+    # Loop over each fold
+    k_errors <- lapply(seq_along(cv.data), function(k) {
+      # Split data in training and validation sets
+      data_train <- dplyr::bind_rows(cv.data[-k])
+      data_valid <- dplyr::bind_rows(cv.data[k])
+
+      # Train mth model on kth training set
+      model_m <- best_subset_classifier(model = models[[m]], data.train = data_train,
+                                        model.family = binomial(link = "probit"),
+                                        model.optimizer = "bobyqa",
+                                        n.iter = 1000000,
+                                        verbose = verbose)
+
+      # Use trained model to make predictions for kth validation set
+      pred_m <- stats::predict(model_m, newdata = data_valid,
+                               type = "response", allow.new.levels = TRUE)
+
+      # Evaluate predictions based on loss function
+      perform_m <- loss_function(pred = pred_m, data.valid = data_valid,
+                                 unit = "individual", measure = "mse",
+                                 y = y, geo.unit = geo.unit)
+    })
+
+    # Mean over all k folds
+    mean(unlist(k_errors))
+  })
+
+  # Choose best-performing model
+  min.m <- which.min(m_errors)
+  out <- models[[min.m]]
+
+  # Function output
+  return(out)
 }
