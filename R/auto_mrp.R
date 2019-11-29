1adee23f756cd5739b711f487561c76e1295029f
Reto Wuest <reto.wuest@unige.ch>
2019-11-29 16:31:34 +0100

    cosmetic changes to main file

--- R/auto_mrp.R
+++ R/auto_mrp.R
@@ -120,14 +120,17 @@ auto_MrP <- function(y, L1.x, L2.x, L2.unit, survey, census,
   # ------------------------------- Create folds -------------------------------
 
   # EBMA hold-out fold
-  ebma_folding_out <- ebma_folding(data = survey, L2.unit = L2.unit,
+  ebma_folding_out <- ebma_folding(data = survey,
+                                   L2.unit = L2.unit,
                                    ebma.size = ebma.size)
 
   ebma_fold <- ebma_folding_out$ebma_fold
   cv_data <- ebma_folding_out$cv_data
 
   # K folds for cross-validation
-  cv_folds <- cv_folding(data = cv_data, k.folds = k.folds,
+  cv_folds <- cv_folding(data = cv_data,
+                         L2.unit = L2.unit,
+                         k.folds = k.folds,
                          cv.sampling = cv.sampling)
 
   # ------------------------ Run individual classifiers ------------------------
