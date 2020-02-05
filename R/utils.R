cd86c1d5766cdb37e243a9ad223f66b5e79663f1
Reto Wuest <reto.wuest@unige.ch>
2020-02-05 19:14:47 +0100

    modify utils functions

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 # Function to create EBMA hold-out fold
-ebma_folding <- function(data, L2.unit, ebma.size = NULL) {
+ebma_folding <- function(data, L2.unit, ebma.size) {
   # Add row number to data frame
   data <- data %>%
     dplyr::mutate(index = row_number())
@@ -10,7 +10,7 @@ ebma_folding <- function(data, L2.unit, ebma.size = NULL) {
 
   # Sample one respondent per geographic unit
   one_per_unit <- lapply(data_list, function(x) {
-    sample(x$index, size = 1)
+    sample(x$index, size = 1, replace = FALSE)
   }) %>%
     unlist()
 
@@ -21,7 +21,8 @@ ebma_folding <- function(data, L2.unit, ebma.size = NULL) {
   data <- data %>%
     dplyr::filter(!(index %in% one_per_unit))
 
-  remainder <- sample(data$index, size = ebma.size - length(one_per_unit))
+  remainder <- sample(data$index, size = ebma.size - length(one_per_unit),
+                      replace = FALSE)
 
   ebma_remainder <- data %>%
     dplyr::filter(index %in% remainder)
@@ -49,28 +50,57 @@ ebma_folding <- function(data, L2.unit, ebma.size = NULL) {
 
 # Function to create CV folds
 cv_folding <- function(data, L2.unit, k.folds,
-                       cv.sampling = c("respondents", "units")) {
+                       cv.sampling = c("respondents", "L2 units")) {
 
   if (cv.sampling == "respondents") {
     # Add row number to data frame
     data <- data %>%
       dplyr::mutate(index = row_number())
 
-    # Create folds
-    fold_indices <- caret::createFolds(data$index, k = k.folds,
-                                       list = TRUE, returnTrain = FALSE)
+    # Randomize indices of respondents
+    indices <- sample(data$index, size = length(data$index), replace = FALSE)
 
+    # Define number of units per fold
+    no_floor <- floor(length(indices) / k.folds)
+    no_remaining <- length(indices) - no_floor * k.folds
+
+    no_fold <- rep(no_floor, times = k.folds)
+
+    if (no_remaining > 0) {
+      no_fold[1:no_remaining] <- no_fold[1:no_remaining] + 1
+    }
+
+    # Split indices into folds
+    fold_indices <- split(indices, rep(1:k.folds, times = no_fold))
+
+    # Partition data according to indices
     out <- lapply(fold_indices, function(x) {
       data %>%
         dplyr::filter(index %in% x) %>%
         dplyr::select(-index)
     })
   } else {
-    # Create folds
-    fold_indices <- data[[L2.unit]] %>%
-      unique() %>%
-      caret::createFolds(k = k.folds, list = TRUE, returnTrain = FALSE)
+    # Extract indices of geographic units
+    indices <- data[[L2.unit]] %>%
+      unique()
 
+    # Randomize order of indices
+    indices <- sample(indices, size = length(indices), replace = FALSE)
+
+    # Define number of units per fold
+    no_floor <- floor(length(indices) / k.folds)
+    no_remaining <- length(indices) - no_floor * k.folds
+
+    no_fold <- rep(no_floor, times = k.folds)
+
+    if (no_remaining > 0) {
+      no_fold[1:no_remaining] <- no_fold[1:no_remaining] + 1
+    }
+
+    # Split indices into folds
+    fold_indices <- split(indices, rep(1:k.folds, times = no_fold))
+
+    # Partition data according to indices
     out <- lapply(fold_indices, function(x) {
       data %>%
         dplyr::filter(.data[[L2.unit]] %in% x)
@@ -83,15 +113,15 @@ cv_folding <- function(data, L2.unit, k.folds,
 
 
 # Function to create model list for best subset classifier
-model_list <- function(y, L1.x, L2.x, L2.unit, L2.re = NULL) {
+model_list <- function(y, L1.x, L2.x, L2.unit, L2.reg = NULL) {
   # Individual-level random effects
   L1_re <- paste(paste("(1 | ", L1.x, ")", sep = ""), collapse = " + ")
 
   # Geographic unit or Geographic unit-Geographic region random effects
-  if (is.null(L2.re)) {
+  if (is.null(L2.reg)) {
     L2_re <- paste("(1 | ", L2.unit, ")", sep = "")
   } else {
-    L2_re <- paste(paste("(1 | ", L2.re, "/", L2.unit, ")", sep = ""),
+    L2_re <- paste(paste("(1 | ", L2.reg, "/", L2.unit, ")", sep = ""),
                    collapse = " + ")
   }
 
@@ -118,16 +148,49 @@ model_list <- function(y, L1.x, L2.x, L2.unit, L2.re = NULL) {
 }
 
 
+# Function to create model list for PCA classifier
+model_list_pca <- function(y, L1.x, L2.x, L2.unit, L2.reg = NULL) {
+  # Individual-level random effects
+  L1_re <- paste(paste("(1 | ", L1.x, ")", sep = ""), collapse = " + ")
+
+  # Geographic unit or Geographic unit-Geographic region random effects
+  if (is.null(L2.reg)) {
+    L2_re <- paste("(1 | ", L2.unit, ")", sep = "")
+  } else {
+    L2_re <- paste(paste("(1 | ", L2.reg, "/", L2.unit, ")", sep = ""),
+                   collapse = " + ")
+  }
+
+  # Combine all random effects
+  all_re <- paste(c(L1_re, L2_re), collapse = " + ")
+
+  # Empty model
+  empty_model <- list(as.formula(paste(y, " ~ ", all_re, sep = "")))
+
+  # Remaining models
+  L2_list <- lapply(seq_along(L2.x), function(x) {L2.x[1:x]})
+  L2_list <- lapply(L2_list, function(x) {
+    as.formula(paste(y, " ~ ", paste(x, collapse = " + "), " + ", all_re, sep = ""))
+    })
+
+  # Combine models in list
+  out <- c(empty_model, L2_list)
+
+  # Function output
+  return(out)
+}
+
+
 # Loss function
 loss_function <- function(pred, data.valid,
-                          unit = c("individual", "L2.unit"),
-                          measure = c("mse", "mae"),
+                          loss.unit = c("individual", "L2.units"),
+                          loss.measure = c("mse", "mae"),
                           y, L2.unit) {
-  if (unit == "individual" & measure == "mse") {
+  if (loss.unit == "individual" & loss.measure == "mse") {
     out <- mean((data.valid[[y]] - pred)^2)
-  } else if (unit == "individual" & measure == "mae") {
+  } else if (loss.unit == "individual" & loss.measure == "mae") {
     out <- mean(abs(data.valid[[y]] - pred))
-  } else if (unit == "L2.unit" & measure == "mse") {
+  } else if (loss.unit == "L2.units" & loss.measure == "mse") {
     data.valid <- data.valid %>%
       dplyr::mutate(pred = pred)
 
