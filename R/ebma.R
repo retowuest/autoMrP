7219a63112b2d70da001a2d7ad0fdfc68ffd0797
Reto Wuest <reto.wuest@unige.ch>
2020-02-09 14:08:56 +0100

    add svm

--- R/ebma.R
+++ R/ebma.R
@@ -1,11 +1,12 @@
 ebma <- function(ebma.fold, L1.x, L2.x, L2.unit, L2.reg, post.strat, Ndraws = 100, 
                  tol.values = c(0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001),
-                 best.subset, pca, lasso, verbose){
+                 best.subset, pca, lasso, gb, verbose){
   
   # models
   model_bs <- post.strat$models$best_subset
   model_pca <- post.strat$models$pca
   model_lasso <- post.strat$models$lasso
+  model_gb <- post.strat$models$gb
   
   # training predictions
   train_preds <- post.strat$predictions$Level1 %>%
@@ -40,11 +41,13 @@ ebma <- function(ebma.fold, L1.x, L2.x, L2.unit, L2.reg, post.strat, Ndraws = 10
         dplyr::ungroup() %>%
         dplyr::mutate_at(.vars = c( L1.x, L2.unit, L2.reg), .funs = as.factor)
       
+      
       # predict outcomes in test set
       test_preds <- dplyr::tibble(
         best_subset = predict(object = model_bs, newdata = test, type = "response", allow.new.levels = TRUE),
         pca = predict(object = model_pca, newdata = test, type = "response", allow.new.levels = TRUE),
-        lasso = as.numeric(predict(object = model_lasso, newdata = data.frame(test), type = "response"))
+        lasso = as.numeric(predict(object = model_lasso, newdata = data.frame(test), type = "response")),
+        gb = gbm::predict.gbm(object = model_gb, newdata = test, n.trees = model_gb$n.trees, type = "response")
       )
       
       # outcome on the test
@@ -88,11 +91,11 @@ ebma <- function(ebma.fold, L1.x, L2.x, L2.unit, L2.reg, post.strat, Ndraws = 10
   
   # average model weights
   final_model_weights <- apply(weights_mat, 2, mean)
-  names(final_model_weights) <- c("BestSubset", "PCA", "Lasso")
+  names(final_model_weights) <- c("BestSubset", "PCA", "Lasso", "GB")
   
   # weighted average
   L2_preds <- post.strat$predictions$Level2 %>%
-    dplyr::mutate(ebma = as.numeric(cbind(best_subset, pca, lasso) %*% final_model_weights))
+    dplyr::mutate(ebma = as.numeric(cbind(best_subset, pca, lasso, gb) %*% final_model_weights))
   
   # function output
   return(L2_preds)
