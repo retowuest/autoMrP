6ffebeece6532b4d054c5d00857285aa9500ba77
Reto Wuest <reto.wuest@unige.ch>
2020-04-05 20:47:30 +0200

    update data folder

---
+++ data/make-data.R
@@ -0,0 +1,40 @@
+library(magrittr)
+
+# Set seed
+set.seed(1234)
+
+# Load Stata data files
+# (Directory of data: "Code/BH data analysis/Data")
+survey_data <- foreign::read.dta("./data/.tmp/file11.dta")
+census_data <- foreign::read.dta("./data/.tmp/census11.dta")
+
+# Select relevant variables
+survey_data <- survey_data %>%
+  dplyr::select(y, age, educ, gXr, state, stateid, region,
+                pvote, religcon, urban, unemp, hispanics, white)
+
+census_data <- census_data %>%
+  dplyr::select(age, educ, gXr, stateid, region,
+                pvote, religcon, urban, unemp, hispanics, white,
+                n)
+
+# Draw a sample of size n_sample from full survey data set
+# (We make sure to have at least n_min respondents per geographic unit)
+n_sample <- 1500
+n_min <- 6
+select_rows <- sample(1:nrow(survey_data), size = n_sample, replace = FALSE)
+survey_sample <- survey_data[select_rows, ]
+
+while(min(table(survey_sample$stateid)) < n_min) {
+  select_rows <- sample(1:nrow(survey_data), size = n_sample, replace = FALSE)
+  survey_sample <- survey_data[select_rows, ]
+}
+
+# Save data files
+save(survey_data, file = "./data/survey_data.RData")
+save(census_data, file = "./data/census_data.RData")
+save(survey_sample, file = "./data/survey_sample.RData")
+
+# Remove data files from workspace
+rm(survey_data, census_data, survey_sample,
+   n_min, n_sample, select_rows)
