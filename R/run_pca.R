6e35c93636f334e53ffcd257e8f413fa7afbcda8
Reto Wuest <reto.wuest@unige.ch>
2020-04-24 17:28:23 +0200

    modify argument names

--- R/run_pca.R
+++ R/run_pca.R
@@ -1,38 +1,44 @@
-#' Apply pca classifier to MrP.
+#' Apply PCA classifier to MrP.
 #'
-#' \code{pca} is a wrapper function that applies the pca classifier to data
+#' \code{run_pca} is a wrapper function that applies the PCA classifier to data
 #' provided by the user, evaluates prediction performance, and chooses the
 #' best-performing model.
 #'
 #' @param y Outcome variable. A character scalar containing the column name of
-#'   the outcome variable.
-#' @param L1.x Individual-level covariates. A character vector of column names
-#'   corresponding to the individual-level variables used to predict the outcome
-#'   variable. Must include geographic unit.
-#' @param L2.x Context-level covariates. A character vector of column names
-#'   corresponding to the context-level variables used to predict the outcome
-#'   variable.
-#' @param L2.unit Geographic unit. A character scalar indicating the column
-#'   name of the geographic unit at which outcomes should be aggregated.
-#' @param L2.reg Geographic region. A character scalar indicating the column
-#'   name of the geographic region by which geographic units are grouped
-#'   (L2.unit must be nested within L2.re).
+#'   the outcome variable in \code{survey}.
+#' @param L1.x Individual-level covariates. A character vector containing the
+#'   column names of the individual-level variables in \code{survey} and
+#'   \code{census} used to predict outcome \code{y}. Note that geographic unit
+#'   is specified in argument \code{L2.unit}.
+#' @param L2.x Context-level covariates. A character vector containing the
+#'   column names of the context-level variables in \code{survey} and
+#'   \code{census} used to predict outcome \code{y}.
+#' @param L2.unit Geographic unit. A character scalar containing the column
+#'   name of the geographic unit in \code{survey} and \code{census} at which
+#'   outcomes should be aggregated.
+#' @param L2.reg Geographic region. A character scalar containing the column
+#'   name of the geographic region in \code{survey} and \code{census} by which
+#'   geographic units are grouped (\code{L2.unit} must be nested within
+#'   \code{L2.reg}). Default is \code{NULL}.
 #' @param loss.unit Loss function unit. A character-valued scalar indicating
-#'   whether the loss should be evaluated at the level of individual respondents
-#'   or the level of geographic units. Default is at the individual level.
-#' @param loss.measure Loss function measure. A character-valued scalar
-#'   indicating whether the loss should be measured by the mean squared error
-#'   or the mean absolute error. Default is the MSE.
-#' @param data Data for cross-validation. A list of k data.frames, one for
-#'   each fold used in k-fold cross-validation.
-#' @param verbose Verbose output. A logical vector indicating whether or not
-#'   verbose output should be printed.
+#'   whether performance loss should be evaluated at the level of individual
+#'   respondents (\code{individuals}) or geographic units (\code{L2 units}).
+#'   Default is \code{individuals}.
+#' @param loss.fun Loss function. A character-valued scalar indicating whether
+#'   prediction loss should be measured by the mean squared error (\code{MSE})
+#'   or the mean absolute error (\code{MAE}). Default is \code{MSE}.
+#' @param data Data for cross-validation. A \code{list} of \eqn{k}
+#'   \code{data.frames}, one for each fold to be used in \eqn{k}-fold
+#'   cross-validation.
+#' @param verbose Verbose output. A logical argument indicating whether or not
+#'   verbose output should be printed. Default is \code{TRUE}.
 #' @return
 #' @examples
 
 pca <- function(y, L1.x, L2.x, L2.unit, L2.reg,
-                loss.unit, loss.measure,
+                loss.unit, loss.fun,
                 data, verbose) {
+
   # List of all models to be evaluated
   models <- model_list_pca(y = y,
                            L1.x = L1.x,
@@ -43,7 +49,7 @@ pca <- function(y, L1.x, L2.x, L2.unit, L2.reg,
   # Train and evaluate each model
   m_errors <- lapply(seq_along(models), function(m) {
     # Print model m
-    if (isTRUE(verbose == TRUE)) {
+    if (isTRUE(verbose)) {
       M <- length(models)
       cat(paste("Best subset: Running model ", m,
                 " out of ", M, " models\n", sep = ""))
@@ -68,10 +74,12 @@ pca <- function(y, L1.x, L2.x, L2.unit, L2.reg,
                                type = "response", allow.new.levels = TRUE)
 
       # Evaluate predictions based on loss function
-      perform_m <- loss_function(pred = pred_m, data.valid = data_valid,
+      perform_m <- loss_function(pred = pred_m,
+                                 data.valid = data_valid,
                                  loss.unit = loss.unit,
-                                 loss.measure = loss.measure,
-                                 y = y, L2.unit = L2.unit)
+                                 loss.fun = loss.fun,
+                                 y = y,
+                                 L2.unit = L2.unit)
     })
 
     # Mean over all k folds
