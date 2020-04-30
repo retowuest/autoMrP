74e6c123272404141250b1fa9c3c3fa386287e75
philippbroniecki <philippbroniecki@gmail.com>
2020-04-30 17:28:25 +0100

    Update post_stratification.R
    post-stratification works now with a selection of classifiers

--- R/post_stratification.R
+++ R/post_stratification.R
@@ -8,9 +8,14 @@ post_stratification <- function(y, L1.x, L2.x, L2.unit, L2.reg,
                                 kernel, mrp.L2.x, data, census,
                                 verbose){
 
+  #browser()
+
   # Copy L2.unit b/c it is needed twice but might be reset depending on call
   # See lines: 59-63
   L2_unit <- L2.unit
+
+  # model container for EBMA
+  models <- list()
 
   # ----- Fit optimal model and make prediction for individual classifiers -----
 
@@ -23,6 +28,19 @@ post_stratification <- function(y, L1.x, L2.x, L2.unit, L2.reg,
                                               model.optimizer = "bobyqa",
                                               n.iter = 1000000,
                                               verbose = verbose)
+
+    # post-stratification
+    bs_preds <- census %>%
+      dplyr::mutate(best_subset = stats::predict(object = best_subset_opt, newdata = census, allow.new.levels = TRUE, type = "response")) %>%
+      dplyr::group_by(.dots = list(L2_unit)) %>%
+      dplyr::summarize(best_subset = weighted.mean(x = best_subset, w = prop))
+
+    # individual level predictions for EBMA
+    bs_ind <- stats::predict(object = best_subset_opt, type = "response")
+
+    # model for EBMA
+    models$best_subset <- best_subset_opt
+
   }
 
   # Classifier 2: Lasso
@@ -45,7 +63,20 @@ post_stratification <- function(y, L1.x, L2.x, L2.unit, L2.reg,
 
     lasso.census <- census
     lasso.census[,y] <- 1
-    lasso_p <- predict(lasso_opt, newdata = data.frame(lasso.census), type = "response")
+    lasso_p <- stats::predict(lasso_opt, newdata = data.frame(lasso.census), type = "response")
+
+    # # post-stratification
+    lasso_preds <- census %>%
+      dplyr::mutate(lasso = lasso_p) %>%
+      dplyr::group_by(.dots = list(L2_unit)) %>%
+      dplyr::summarize(lasso = weighted.mean(x = lasso, w = prop))
+
+    # individual level predictions for EBMA
+    lasso_ind <- stats::predict(object = lasso_opt, type = "response")
+
+    # model for EBMA
+    models$lasso <- lasso_opt
+
   }
 
   # Classifier 3: PCA
@@ -57,6 +88,20 @@ post_stratification <- function(y, L1.x, L2.x, L2.unit, L2.reg,
                                       model.optimizer = "bobyqa",
                                       n.iter = 1000000,
                                       verbose = verbose)
+
+    # post-stratification
+    pca_preds <- census %>%
+      dplyr::mutate(pca = stats::predict(object = pca_opt, newdata = census, allow.new.levels = TRUE, type = "response")) %>%
+      dplyr::group_by(.dots = list(L2_unit)) %>%
+      dplyr::summarize(pca = weighted.mean(x = pca, w = prop))
+
+    # individual level predictions for EBMA
+    pca_ind <- stats::predict(object = pca_opt, type = "response")
+
+    # model for EBMA
+    models$pca <- pca_opt
+
+
   }
 
   # Classifier 4: GB
@@ -88,6 +133,19 @@ post_stratification <- function(y, L1.x, L2.x, L2.unit, L2.reg,
                             n.minobsinnode = n.minobsinnode,
                             shrinkage = gb.opt$shrinkage,
                             verbose = verbose)
+
+    # post-stratification
+    gb_preds <- census %>%
+      dplyr::mutate(gb = gbm::predict.gbm(object = gb_opt, newdata = census, n.trees = gb.opt$n_trees, type = "response")) %>%
+      dplyr::group_by(.dots = list(L2_unit)) %>%
+      dplyr::summarize(gb = weighted.mean(x = gb, w = prop))
+
+    # individual level predictions for EBMA
+    gb_ind <- gbm::predict.gbm(object = gb_opt, n.trees = gb.opt$n_trees, type = "response")
+
+    # model for EBMA
+    models$gb <- gb_opt
+
   }
 
   # Classifier 5: SVM
@@ -122,6 +180,22 @@ post_stratification <- function(y, L1.x, L2.x, L2.unit, L2.reg,
                    probability = TRUE)
       ))
     }
+
+    # post-stratification
+    svm_preds <- census %>%
+      dplyr::mutate(svm = attr(stats::predict(object = svm_opt, newdata = census, probability = TRUE),"probabilities")[,"1"]) %>%
+      dplyr::group_by(.dots = list(L2_unit)) %>%
+      dplyr::summarize(svm = weighted.mean(x = svm, w = prop))
+
+    # individual level predictions for EBMA
+    attr(stats::predict(object = svm_opt, newdata = data, probability = TRUE),"probabilities")[,"1"]
+
+    # individual level predictions for EBMA
+    svm_ind <- attr(stats::predict(object = svm_opt, newdata = data, probability = TRUE),"probabilities")[,"1"]
+
+    # model for EBMA
+    models$svm <- svm_opt
+
   }
 
   # Classifier 6: MRP
@@ -165,40 +239,59 @@ post_stratification <- function(y, L1.x, L2.x, L2.unit, L2.reg,
     }
   }
 
-  # --------------------------- Post-stratification ----------------------------
+  # --------------------------- combine l2 level predictions ----------------------------
 
-  # post-stratification: 1) predict, 2) weighted mean by state
-  L2_preds <- census %>%
-    dplyr::mutate(best_subset = predict(object = model_bs, newdata = census, allow.new.levels = TRUE, type = "response"),
-                  pca = predict(object = model_pca, newdata = census, allow.new.levels = TRUE, type = "response"),
-                  lasso = lasso_p,
-                  gb = gbm::predict.gbm(object = model_gb, newdata = census, n.trees = gb$n_trees, type = "response"),
-                  svm = attr(predict(object = model_svm, newdata = census, probability = TRUE),"probabilities")[,"1"]) %>%
-    dplyr::group_by(.dots = list(L2_unit)) %>%
-    dplyr::summarize(best_subset = weighted.mean(x = best_subset, w = prop),
-                     pca = weighted.mean(x = pca, w = prop),
-                     lasso = weighted.mean(x = lasso, w = prop),
-                     gb = weighted.mean(x = gb, w = prop),
-                     svm = weighted.mean(x = svm, w = prop))
+  # tibble of L2 units
+  L2_preds <- dplyr::select(census, one_of(L2.unit)) %>%
+    dplyr::distinct()
+  # add existing classifiers
+  if(exists("bs_preds")) L2_preds <- dplyr::left_join(x = L2_preds, y = bs_preds, by = L2.unit)
+  if(exists("lasso_preds")) L2_preds <- dplyr::left_join(x = L2_preds, y = lasso_preds, by = L2.unit)
+  if(exists("pca_preds")) L2_preds <- dplyr::left_join(x = L2_preds, y = pca_preds, by = L2.unit)
+  if(exists("gb_preds")) L2_preds <- dplyr::left_join(x = L2_preds, y = gb_preds, by = L2.unit)
+  if(exists("svm_preds")) L2_preds <- dplyr::left_join(x = L2_preds, y = svm_preds, by = L2.unit)
 
 
   # individual predictions for EBMA
-  L1_preds <- dplyr::tibble(
-    best_subset = predict(object = model_bs, type = "response"),
-    pca = predict(object = model_pca, type = "response"),
-    lasso = as.numeric(predict(object = model_l, type = "response")),
-    gb = gbm::predict.gbm(object = model_gb, n.trees = gb$n_trees, type = "response"),
-    svm = attr(predict(object = model_svm, newdata = data, probability = TRUE),"probabilities")[,"1"]
-  )
-  L1_preds <- L1_preds %>%
-    dplyr::mutate(!!y := as.numeric(unlist(data[,y]))) %>%
-    dplyr::select(one_of(y), "best_subset", "pca", "lasso", "gb", "svm")
+  L1_preds <- data %>%
+    dplyr::select(one_of(y)) %>%
+    # add best subset
+    dplyr::mutate(
+      best_subset = if(exists("bs_ind")){
+        as.numeric(bs_ind)
+      } else{
+        NA
+        },
+      # add lasso
+      lasso = if(exists("lasso_ind")){
+        as.numeric(lasso_ind)
+        } else{
+          NA
+          },
+      # add pca
+      pca = if(exists("pca_ind")){
+        as.numeric(pca_ind)
+      } else{
+        NA
+      },
+      # add gb
+      gb = if(exists("gb_ind")){
+        as.numeric(gb_ind)
+      } else{
+        NA
+      },
+      # add svm
+      svm = if(exists("svm_ind")){
+        as.numeric(svm_ind)
+      } else{
+        NA
+      }
+    )
+  # remove NA's
+  L1_preds <- L1_preds[,apply(X = L1_preds, MARGIN = 2, FUN = function(x){
+    all(!is.na(x))})]
 
   # Function output
   return(ps = list(predictions = list(Level1 = L1_preds, Level2 = L2_preds),
-                   models = list(best_subset = model_bs,
-                                 pca = model_pca,
-                                 lasso = model_l,
-                                 gb = model_gb,
-                                 svm = model_svm)))
+                   models = models))
 }
