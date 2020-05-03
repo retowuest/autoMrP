fbb4940714697956ddbca7e65dc40c79f351b644
philippbroniecki <philippbroniecki@gmail.com>
2020-05-03 21:06:05 +0100

    Update post_stratification.R

--- R/post_stratification.R
+++ R/post_stratification.R
@@ -310,10 +310,12 @@ post_stratification <- function(y, L1.x, L2.x, L2.unit, L2.reg,
     all_re <- paste(c(L1_re, L2_re), collapse = " + ")
 
     # Context-level fixed effects
-    if(mrp.L2.x != "empty") {
-      L2_fe <- paste(mrp.L2.x, collapse = " + ")
-    } else{
-      L2_fe <- ""
+    if(!is.null(mrp.L2.x)){
+      if(mrp.L2.x != "empty") {
+        L2_fe <- paste(mrp.L2.x, collapse = " + ")
+      } else{
+        L2_fe <- ""
+      }
     }
 
     # std MrP formula
