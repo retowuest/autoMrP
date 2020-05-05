c5535dc66960946f5cc264b736d257645e6442ec
Reto Wuest <reto.wueest@uib.no>
2020-05-05 14:07:49 +0200

    update autoMrP

--- R/survey_data.R
+++ R/survey_data.R
@@ -12,7 +12,7 @@
 #'   \item{L1x1}{Age group (four categories: 1 = 18-29; 2 = 30-44; 3 = 45-64; 4 = 65+)}
 #'   \item{L1x2}{Education level (four categories: 1 = < high school; 2 = high school graduate; 3 = some college; 4 = college graduate)}
 #'   \item{L1x3}{Gender-race combination (six categories: 1 = white male; 2 = black male; 3 = hispanic male; 4 = white female; 5 = black female; 6 = hispanic female)}
-#'   \item{state}{The state that the respondent is from (four categories: )}
+#'   \item{state}{The state that the respondent is from (four categories: 1 = Northeast; 2 = Midwest; 3 = South; 4 = West)}
 #'   \item{L2.unit}{An aditional  variable indicating the state that the respondent is from}
 #'   \item{region}{Region in the U.S. that the respondent is from; 1 = something, 2 = something, 3 = something, 4 = something}
 #'   \item{L2.x1}{The share of votes for the Republican candidate in the previous presidential election}
